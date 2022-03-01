cc5675cc558099dd687f72e4fd70ceb048d6c4a3
sylarBrest <aostrovsky87@gmail.com>
2022-03-01 10:27:24 +0300

    feat: add dns-stats solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let res = [], dnsStats = {};
+  domains = domains.map(el => el.split('.').reverse())
+                   .forEach(el => {
+                     let dns = '';
+                     for (word of el) {
+                       dns += `.${word}`;
+                       res.push(dns);
+                     }  
+                   });
+  for (dns of res)
+    (dnsStats.hasOwnProperty(dns)) ? dnsStats[dns]++ : dnsStats[dns] = 1;
+  return dnsStats;
 }
 
 module.exports = {
